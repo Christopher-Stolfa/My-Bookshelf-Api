1fa5c8a9769d463a53d83df62ed642957579fe6b
Chribtoper <xcstolfax@gmail.com>
2022-01-16 11:32:41 -0700

    Moved business logic from books controller into googleBooks services, searchBooks now sets default params in case there are no params and passes a cleaned up obj back to the controller or an error. bookSearch is now an async function that handles external api calls in a try catch

--- Controllers/books.controllers.js
+++ Controllers/books.controllers.js
@@ -1,25 +1,25 @@
 const googleBooks = require("../Services/googleBooks.services");
 
-const bookSearch = (req, res) => {
+const bookSearch = async (req, res) => {
   const { searchQuery, maxResults, startIndex, orderBy } = JSON.parse(
     req.query.data
   );
-  console.log(searchQuery);
-
-  googleBooks
-    .searchBooks(searchQuery, maxResults, startIndex, orderBy)
-    .then((response) => {
-      res.status(200).json({
-        message: "Search successful.",
-        bookSearchData: response.data.items.map((item) => item),
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        message: err.toString(),
-      });
+  try {
+    const bookSearchData = await googleBooks.searchBooks(
+      searchQuery,
+      maxResults,
+      startIndex,
+      orderBy
+    );
+    res.status(200).json({
+      message: "Search successful.",
+      bookSearchData
     });
+  } catch (err) {
+    res.status(500).json({
+      message: err.toString()
+    });
+  }
 };
 
 module.exports = { bookSearch };
--- Services/googlebooks.services.js
+++ Services/googlebooks.services.js
@@ -6,7 +6,46 @@ const googleVolumesUri = "https://www.googleapis.com/books/v1/volumes";
 
 const searchBooks = (query, maxResults, startIndex, orderBy) => {
   const url = `${googleVolumesUri}?q=${query}&maxResults=${maxResults}&orderBy=${orderBy}&startIndex=${startIndex}&key=${apiKey}`;
-  return axios.get(url);
+  return new Promise((resolve, reject) => {
+    axios
+      .get(url)
+      .then(res => {
+        resolve(
+          res.data.items.map(
+            ({
+              id,
+              volumeInfo: {
+                title = "",
+                description = "",
+                authors = [""],
+                publisher = "",
+                publishedDate = "",
+                pageCount = -1,
+                averageRating = -1,
+                ratingsCount = -1,
+                imageLinks = [""],
+                language = "",
+                categories = ["No information on this listing."]
+              }
+            }) => ({
+              id,
+              title,
+              description,
+              authors,
+              publisher,
+              publishedDate,
+              pageCount,
+              averageRating,
+              ratingsCount,
+              imageLinks,
+              language,
+              categories
+            })
+          )
+        );
+      })
+      .catch(err => reject(err));
+  });
 };
 
 module.exports = {
