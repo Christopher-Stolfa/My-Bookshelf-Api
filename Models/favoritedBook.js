57e0e4115627eca35872a1bc1bbdd28b5b652e2b
Chribtoper <xcstolfax@gmail.com>
2022-01-30 01:44:54 -0700

    model now takes image src as a text field instead of string in case google api provides a long text img src

--- Models/favoritedBook.js
+++ Models/favoritedBook.js
@@ -9,7 +9,7 @@ const FavoritedBook = sequelize.define(
       autoIncrement: true,
       allowNull: false,
       unique: true,
-      primaryKey: true
+      primaryKey: true,
     },
     GoogleBooksId: {
       type: Sequelize.STRING,
@@ -17,15 +17,15 @@ const FavoritedBook = sequelize.define(
       validate: {
         async exists(googleBooksId) {
           const book = await FavoritedBook.findOne({
-            where: { GoogleBooksId: googleBooksId, UserId: this.UserId }
+            where: { GoogleBooksId: googleBooksId, UserId: this.UserId },
           });
           if (book) throw new Error("Item already in favorites");
         },
         notEmpty: {
           args: true,
-          msg: "Id field cannot be empty."
-        }
-      }
+          msg: "Id field cannot be empty.",
+        },
+      },
     },
     Title: {
       type: Sequelize.STRING,
@@ -33,13 +33,13 @@ const FavoritedBook = sequelize.define(
       validate: {
         notEmpty: {
           args: true,
-          msg: "Title field cannot be empty."
-        }
-      }
+          msg: "Title field cannot be empty.",
+        },
+      },
     },
     Description: {
       type: Sequelize.TEXT,
-      allowNull: false
+      allowNull: false,
     },
     Authors: {
       type: Sequelize.STRING,
@@ -49,53 +49,53 @@ const FavoritedBook = sequelize.define(
       },
       set(authors) {
         this.setDataValue("Authors", authors.join(";"));
-      }
+      },
     },
     Publisher: {
       type: Sequelize.STRING,
-      allowNull: false
+      allowNull: false,
     },
     PublishedDate: {
       type: Sequelize.STRING,
-      allowNull: false
+      allowNull: false,
     },
     PageCount: {
       type: Sequelize.INTEGER,
-      allowNull: false
+      allowNull: false,
     },
     AverageRating: {
       type: Sequelize.FLOAT,
-      allowNull: false
+      allowNull: false,
     },
     RatingsCount: {
       type: Sequelize.INTEGER,
-      allowNull: false
+      allowNull: false,
     },
     Language: {
       type: Sequelize.STRING,
-      allowNull: false
+      allowNull: false,
     },
     ImageLink: {
-      type: Sequelize.STRING,
-      allowNull: false
+      type: Sequelize.TEXT,
+      allowNull: false,
     },
     Categories: {
-      type: Sequelize.STRING,
+      type: Sequelize.TEXT,
       allowNull: false,
       get() {
         return this.getDataValue("Categories").split(";");
       },
       set(categories) {
         this.setDataValue("Categories", categories.join(";"));
-      }
-    }
+      },
+    },
   },
   {
     hooks: {
       validationFailed: (instance, options, { errors }) => {
         throw { message: "Error favoriting book", code: 500 };
-      }
-    }
+      },
+    },
   }
 );
 
