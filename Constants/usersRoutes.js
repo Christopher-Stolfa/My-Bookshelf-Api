20dc7bcd4d0d412fce2abe9d5b46741554ad980c
Chribtoper <xcstolfax@gmail.com>
2022-02-02 20:28:15 -0700

    Added a constant for a user fetching favorited books

--- Constants/usersRoutes.js
+++ Constants/usersRoutes.js
@@ -7,12 +7,14 @@ const CHECK_SESSION = "/check-session";
 // User book routes
 const SAVE_FAVORITED_BOOK = "/save-favorited-book";
 const REMOVED_FAVORITED_BOOK = "/remove-favorited-book";
+const GET_FAVORITED_BOOKS = "/get-favorited-books";
 
 module.exports = {
-    SIGN_OUT,
-    SIGN_UP,
-    SIGN_IN,
-    CHECK_SESSION,
-    SAVE_FAVORITED_BOOK,
-    REMOVED_FAVORITED_BOOK
-}
+  SIGN_OUT,
+  SIGN_UP,
+  SIGN_IN,
+  CHECK_SESSION,
+  SAVE_FAVORITED_BOOK,
+  REMOVED_FAVORITED_BOOK,
+  GET_FAVORITED_BOOKS
+};
