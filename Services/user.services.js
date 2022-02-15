d07592ca167a45c9f35486d7e60c9ea3417da2d1
Chribtoper <xcstolfax@gmail.com>
2022-02-15 02:08:04 -0700

    On user register, now uses regex to check if the password passes validation. Created SendPasswordReset function that sets the reset token in the User data and then sends a link with the token to the users email for their password reset

--- Services/user.services.js
+++ Services/user.services.js
@@ -4,7 +4,18 @@ const crypto = require("crypto");
 const User = require("../Models/user");
 const FavoritedBook = require("../Models/favoritedBook");
 
-const createUser = ({ displayName, firstName, lastName, email, password }) =>
+const createUser = ({ displayName, firstName, lastName, email, password }) => {
+  if (
+    !/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$/.test(
+      password
+    )
+  ) {
+    throw {
+      message:
+        "Password must have between 8 and 16 characters with at least one uppercase letter, one lowercase letter, one number and one special character",
+      code: 400,
+    };
+  }
   User.create({
     DisplayName: displayName,
     FirstName: firstName,
@@ -22,6 +33,7 @@ const createUser = ({ displayName, firstName, lastName, email, password }) =>
     };
     return userData;
   });
+};
 
 const findUserByEmail = (email) =>
   User.findOne({ where: { Email: email } }).then((user) => user);
@@ -95,7 +107,42 @@ const getFavoritedBooks = (userId) =>
       )
   );
 
-const sendPasswordReset = () => {};
+const sendPasswordReset = (user) => {
+  const token = crypto.randomBytes(20).toString("hex");
+  user.update({
+    ResetPasswordToken: token,
+    ResetPasswordExpires: Date.now() + 3600000,
+  });
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: `${process.env.TRANSPORTER_EMAIL}`,
+      pass: `${process.env.TRANSPORTER_PASSWORD}`,
+    },
+  });
+
+  const mailOptions = {
+    from: "MyBookshelfNoReply@gmail.com",
+    to: `${user.Email}`,
+    subject: "Link To Reset Your MyLibrary Password",
+    text:
+      "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
+      "Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n" +
+      `${process.env.LOCALHOST_CLIENT_ORIGIN}/reset/${token}\n\n` +
+      "If you did not request this, please ignore this email and your password will remain unchanged.\n",
+  };
+
+  transporter.sendMail(mailOptions, (error, response) => {
+    if (error) {
+      console.error("there was an error: ", error);
+      throw error;
+    } else {
+      console.log("here is the res: ", response);
+      user.save();
+    }
+  });
+};
 
 module.exports = {
   createUser,
@@ -104,4 +151,5 @@ module.exports = {
   saveFavoritedBook,
   getFavoritedBooks,
   removeFavoritedBook,
+  sendPasswordReset,
 };
