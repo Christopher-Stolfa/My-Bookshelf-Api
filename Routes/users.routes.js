8f8a0bfa4ef13f66636b7c481d1b9a5e9dbf129a
Chribtoper <xcstolfax@gmail.com>
2022-02-25 19:56:22 -0700

    Client can now trigger a route for update-password

--- Routes/users.routes.js
+++ Routes/users.routes.js
@@ -9,6 +9,7 @@ const {
   CHECK_RESET_TOKEN,
   FORGOT_PASSWORD,
   UPDATE_PASSWORD_WITH_TOKEN,
+  UPDATE_PASSWORD,
 } = require("../Constants/usersRoutes");
 
 router.post(SIGN_OUT, userControllers.userSignOut);
@@ -16,6 +17,7 @@ router.post(SIGN_UP, userControllers.userSignUp);
 router.post(SIGN_IN, userControllers.userSignIn);
 router.post(FORGOT_PASSWORD, userControllers.userForgotPassword);
 router.put(UPDATE_PASSWORD_WITH_TOKEN, userControllers.updatePasswordWithToken);
+router.put(UPDATE_PASSWORD, userControllers.updatePassword);
 router.get(CHECK_SESSION, userControllers.userCheckSession);
 router.get(CHECK_RESET_TOKEN, userControllers.userCheckResetToken);
 
