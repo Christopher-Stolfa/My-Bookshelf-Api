310f3509cb8d50f89b124c9fe07dd166e17016c7
Chribtoper <xcstolfax@gmail.com>
2022-02-28 02:36:25 -0700

    Attempt to fix issue with nginx

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 const PORT = 5000;
 
 app.use(express.json());
-app.set("trust proxy", true);
+app.set("trust proxy", 1);
 app.use(
   cors({
     origin: [process.env.CLIENT_ORIGIN],
