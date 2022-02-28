65cc737a5728119278d49a5e1648f403a76c838e
Chribtoper <xcstolfax@gmail.com>
2022-02-28 03:12:05 -0700

    log just the req

--- src/Controllers/users.controllers.js
+++ src/Controllers/users.controllers.js
@@ -24,7 +24,7 @@ const userSignUp = async (req, res, next) => {
 };
 
 const userCheckSession = async (req, res) => {
-  console.log(req.session);
+  console.log(req);
   if (req.session.user) {
     res.status(200).json({
       message: "Login session exists",
