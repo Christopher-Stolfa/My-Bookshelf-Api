69c64e113f615c44203203a968af3df35d0466fc
Chribtoper <xcstolfax@gmail.com>
2022-03-04 13:20:09 -0700

    Set the link for password reset to the production origin

--- src/Services/user.services.js
+++ src/Services/user.services.js
@@ -3,7 +3,7 @@ import Sequelize from "sequelize";
 import crypto from "crypto";
 import User from "../Models/user.js";
 
-const validatePassword = (password) =>
+const validatePassword = password =>
   /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$/.test(
     password
   );
@@ -13,7 +13,7 @@ const createUser = ({ displayName, firstName, lastName, email, password }) => {
     throw {
       message:
         "Password must have between 8 and 16 characters with at least one uppercase letter, one lowercase letter, one number and one special character",
-      code: 400,
+      code: 400
     };
   }
   User.create({
@@ -21,41 +21,41 @@ const createUser = ({ displayName, firstName, lastName, email, password }) => {
     FirstName: firstName,
     LastName: lastName,
     Email: email,
-    Password: password,
-  }).then((resultData) => {
+    Password: password
+  }).then(resultData => {
     const { UserId, DisplayName, FirstName, LastName, Email } = resultData;
     const userData = {
       userId: UserId,
       email: Email,
       displayName: DisplayName,
       firstName: FirstName,
-      lastName: LastName,
+      lastName: LastName
     };
     return userData;
   });
 };
 
-const findUserByResetToken = (token) =>
+const findUserByResetToken = token =>
   User.findOne({
     where: {
       ResetPasswordToken: token,
       ResetPasswordExpires: {
-        [Sequelize.Op.gt]: Date.now(),
-      },
-    },
-  }).then((user) => {
+        [Sequelize.Op.gt]: Date.now()
+      }
+    }
+  }).then(user => {
     if (user === null) {
       throw {
         message: "Password reset link is invalid or has expired",
-        code: 403,
+        code: 403
       };
     } else {
       return user.Email;
     }
   });
 
-const findUserByEmail = (email) =>
-  User.findOne({ where: { Email: email } }).then((user) => user);
+const findUserByEmail = email =>
+  User.findOne({ where: { Email: email } }).then(user => user);
 
 const userPasswordValid = (passwordToCheck, correctPassword) =>
   User.prototype.validPassword(passwordToCheck, correctPassword);
@@ -66,21 +66,21 @@ const updatePasswordViaToken = (token, email, password) =>
       Email: email,
       ResetPasswordToken: token,
       ResetPasswordExpires: {
-        [Sequelize.Op.gt]: Date.now(),
-      },
-    },
-  }).then((user) => {
+        [Sequelize.Op.gt]: Date.now()
+      }
+    }
+  }).then(user => {
     if (user === null) {
       throw {
         message: "Password reset link is invalid or has expired",
-        code: 403,
+        code: 403
       };
     } else if (user !== null) {
       console.log("user exists in db");
       user.update({
         Password: password,
         ResetPasswordToken: null,
-        ResetPasswordExpires: null,
+        ResetPasswordExpires: null
       });
     } else {
       throw { message: "User does not exist", code: 401 };
@@ -90,20 +90,20 @@ const updatePasswordViaToken = (token, email, password) =>
 const dbUpdatePassword = (user, newPassword) =>
   user.update({ Password: newPassword });
 
-const sendPasswordReset = (user) => {
+const sendPasswordReset = user => {
   const token = crypto.randomBytes(20).toString("hex");
   // Token expires in 10 minutes
   user.update({
     ResetPasswordToken: token,
-    ResetPasswordExpires: Date.now() + 600000,
+    ResetPasswordExpires: Date.now() + 600000
   });
 
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
       user: `${process.env.TRANSPORTER_EMAIL}`,
-      pass: `${process.env.TRANSPORTER_PASSWORD}`,
-    },
+      pass: `${process.env.TRANSPORTER_PASSWORD}`
+    }
   });
 
   const mailOptions = {
@@ -113,8 +113,8 @@ const sendPasswordReset = (user) => {
     text:
       "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
       "Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n" +
-      `${process.env.CLIENT_ORIGIN}/reset/${token}\n\n` +
-      "If you did not request this, please ignore this email and your password will remain unchanged.\n",
+      `${process.env.PROD_ORIGIN}/reset/${token}\n\n` +
+      "If you did not request this, please ignore this email and your password will remain unchanged.\n"
   };
 
   transporter.sendMail(mailOptions, (error, response) => {
@@ -135,5 +135,5 @@ export {
   sendPasswordReset,
   findUserByResetToken,
   updatePasswordViaToken,
-  dbUpdatePassword,
+  dbUpdatePassword
 };
