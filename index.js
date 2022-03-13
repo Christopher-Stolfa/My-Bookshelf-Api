5b87a90ff01c0ee9c934045bb66742f62c041908
Chribtoper <xcstolfax@gmail.com>
2022-03-12 20:24:11 -0700

    File linted, double quotes are now single quotes, unused variables are removed, and added development/production env variables. If the NODE_ENV is PRODUCTION, app will use the production origin and use a path to index.html to handle non api routes, otherwise app will use localhost as the origin and not use the middleware that paths to index.html

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-import "dotenv/config";
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import helmet from "helmet";
-import path from "path";
-import multer from "multer";
-import { fileURLToPath } from "url";
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import path from 'path';
+import multer from 'multer';
+import { fileURLToPath } from 'url';
 
-import { sessionMiddleware } from "./src/Config/redisConfig.js";
+import { sessionMiddleware } from './src/Config/redisConfig.js';
 
 // Error handlers
-import {
-  errorLogger,
-  errorResponder,
-  failSafeHandler,
-} from "./src/Middleware/errorHandler.js";
+import { errorLogger, errorResponder, failSafeHandler } from './src/Middleware/errorHandler.js';
 
 // Routers
-import usersRouter from "./src/Routes/users.routes.js";
-import booksRouter from "./src/Routes/books.routes.js";
-import quotesRouter from "./src/Routes/quotes.routes.js";
-import sequelize from "./src/Config/databaseConfig.js";
+import usersRouter from './src/Routes/users.routes.js';
+import booksRouter from './src/Routes/books.routes.js';
+import quotesRouter from './src/Routes/quotes.routes.js';
+import sequelize from './src/Config/databaseConfig.js';
 
 const app = express();
+const PRODUCTION = 'production';
 const PORT = 5000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const root = path.join(__dirname, "build");
+const root = path.join(__dirname, 'build');
 const upload = multer();
 
 // app.set("trust proxy", 1);
@@ -36,8 +33,8 @@ app.use(
     useDefaults: true,
     directives: {
       ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-      "script-src": ["'self'", "'unsafe-inline'"],
-      "img-src": ["'self'", "https: data:", "books.google.com"],
+      'script-src': ["'self'", "'unsafe-inline'"],
+      'img-src': ["'self'", 'https: data:', 'books.google.com'],
     },
   })
 );
@@ -48,28 +45,32 @@ app.use(sessionMiddleware);
 app.use(
   cors({
     optionsSuccessStatus: 200,
-    origin: process.env.PROD_ORIGIN,
-    methods: ["GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"],
+    origin: process.env.NODE_ENV === PRODUCTION ? process.env.PROD_ORIGIN : process.env.DEV_ORIGIN,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS'],
     credentials: true,
   })
 );
+
 // Uses routes defined in usersRouter alongside /users
 // Example: /users/sign-up, /users/sign-in
-app.use("/api/users", usersRouter);
+app.use('/api/users', usersRouter);
 
 // Uses routes defined in booksRouter alongside /books
 // Example: /books/, /books/book-search
-app.use("/api/books", booksRouter);
+app.use('/api/books', booksRouter);
 
 // Uses routes defined in quotesRouter alongside /quotes
 // Example: /quotes/, /quotes/get-random-qoute
-app.use("/api/quotes", quotesRouter);
+app.use('/api/quotes', quotesRouter);
 
-app.use(express.static(root));
-
-app.use("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// Middlewares that are only used in production.
+if (process.env.NODE_ENV === PRODUCTION) {
+  // use the route to client index to handle non API routes.
+  app.use(express.static(root));
+  app.use('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
 
 // Uses Error handlers
 app.use(errorLogger);
@@ -79,8 +80,8 @@ app.use(failSafeHandler);
 // Checks the database for the Model Schemas and creates tables for them if they don't exist.
 sequelize
   .sync()
-  .then((result) => {
-    console.log("Synced Schemas");
+  .then(() => {
+    console.log('Synced Schemas');
   })
   .catch((err) => {
     console.log(err);
