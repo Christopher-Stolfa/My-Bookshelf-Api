58da44ea16bcdd33e32b5a48aa0c2ae0b9526070
Chribtoper <xcstolfax@gmail.com>
2022-03-14 21:00:24 -0700

    Moved database related files into database folder

---
+++ database/config.js
@@ -0,0 +1,9 @@
+const profiles = require('../config');
+
+const dbConfigs = {};
+
+Object.keys(profiles).forEach((profile) => {
+  dbConfigs[profile] = { ...profiles[profile].database };
+});
+
+module.exports = dbConfigs;
