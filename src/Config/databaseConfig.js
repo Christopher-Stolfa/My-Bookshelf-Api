039c5b60e14583884902f2ac7e2e4b2c4ea8f50d
Chribtoper <xcstolfax@gmail.com>
2022-03-14 18:54:56 -0700

    Shortened filename to dbConfig from databaseConfig

--- src/Config/databaseConfig.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize(process.env.MYSQL_DB_NAME, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
-  dialect: 'mysql',
-  logging: false,
-  host: process.env.SERVER_IP,
-});
-
-module.exports = sequelize;
