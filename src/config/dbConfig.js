411581f901a03107821cbfb8a72bbc5f0cdef0c0
Chribtoper <xcstolfax@gmail.com>
2022-03-16 13:00:42 -0700

    Added host property in the sequelize input

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -4,6 +4,7 @@ const config = require('config');
 const dbConfig = config.get('database');
 
 const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  host: dbConfig.host,
   dialect: dbConfig.dialect,
   logging: false,
 });
