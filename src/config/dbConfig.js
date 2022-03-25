b9849386b42757bea6e9e722af58002d922f8d05
Chribtoper <xcstolfax@gmail.com>
2022-03-24 20:01:47 -0700

    Realized comments are really needed here thus removed them

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -5,23 +5,8 @@
 const Sequelize = require('sequelize');
 const config = require('config');
 
-/**
- * @description Database object from the config file
- * @typedef dbConfig
- * @type {object}
- * @property {string} username - username
- * @property {string} password - password
- * @property {string} database - database name
- * @property {string} host - host address
- * @property {string} dialect - query language dialect, most likely mySQL
- * @property {boolean} logging - option for if we want sequelize to print logs
- */
 const dbConfig = config.get('database');
 
-/**
- * @description
- * @type {Object} - Initialization of sequelize with database configuration params
- */
 const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
   host: dbConfig.host,
   dialect: dbConfig.dialect,
--- src/config/storeConfig.js
+++ src/config/storeConfig.js
@@ -4,22 +4,8 @@
  */
 const session = require('express-session');
 
-/**
- * @type {number} - time a session should expire
- */
 const SESSION_EXPIRATION = 24 * 60 * 60 * 1000;
 
-/**
- * @description - session config object
- * @typedef sessionConfig
- * @type {object}
- * @property {string} key - key name of session
- * @property {boolean} saveUninitialized - defaults as true
- * @property {string} secret - session secret key, should be an env variable
- * @property {boolean} resave - defaults as false
- * @property {object} cookie - cookie options
- * @property {object} cookie.expires - expiration time for cookie
- */
 const sessionConfig = {
   key: 'user-session',
   saveUninitialized: true,
@@ -30,9 +16,6 @@ const sessionConfig = {
   },
 };
 
-/**
- * @definition - if the environment is production, use redis as a data store
- */
 if (process.env.NODE_ENV === 'production') {
   const redis = require('redis');
   const RedisStore = require('connect-redis')(session);
@@ -43,10 +26,6 @@ if (process.env.NODE_ENV === 'production') {
   sessionConfig.store = new RedisStore({ client: redisClient });
 }
 
-/**
- * @definition - session middleware that is instantiated with session config
- * @type {object}
- */
 const sessionMiddleware = session(sessionConfig);
 
 module.exports = sessionMiddleware;
