d491a406e95f947a00365d144f59f6edc4546a90
Chribtoper <xcstolfax@gmail.com>
2022-03-26 18:28:45 -0700

    Added comments and refactored most functions, reducing code by using async/awaits

--- src/services/bookService.js
+++ src/services/bookService.js
@@ -1,6 +1,7 @@
 /**
  * @description - Book services used by the controller for business logic involving database models
  * @module services/bookService
+ * @see module:controllers/bookController
  */
 const { User, Note, FavoritedBook } = require('../database/models');
 
@@ -53,6 +54,7 @@ const dbSaveFavoritedBook = async (userId, book) => {
 };
 
 /**
+ * @todo - Use favoritedBookId instead of using the entire book object
  * @description - Removes a FavoritedBook with a userId foreign key
  * @function dbRemoveFavoritedBook
  * @param {number} userId - Id of the user from their current session
@@ -71,6 +73,7 @@ const dbRemoveFavoritedBook = async (userId, book) => {
 };
 
 /**
+ * @todo - Use favoritedBookId instead of using the entire book object
  * @description - Gets a FavoritedBook by bookId with a userId foreign key
  * @function dbGetFavoritedBook
  * @param {number} userId - Id of the user from their current session
@@ -78,162 +81,134 @@ const dbRemoveFavoritedBook = async (userId, book) => {
  * @throws {error} - Throws an error if the user isn't found, the book isn't found, or if something goes wrong within the model or database itself
  * @returns {Object} - Returns an object with the book data that was just saved
  */
-const dbGetFavoritedBook = (userId, bookId) =>
-  FavoritedBook.findOne({
-    where: { UserId: userId, GoogleBooksId: bookId },
-  }).then((book) => {
-    if (!book) {
-      throw { message: "Book doesn't exist", code: 404 };
-    } else {
-      return {
-        googleBooksId: book.GoogleBooksId,
-        title: book.Title,
-        description: book.Description,
-        authors: book.Authors,
-        publisher: book.Publisher,
-        publishedDate: book.PublishedDate,
-        pageCount: book.PageCount,
-        averageRating: book.AverageRating,
-        ratingsCount: book.RatingsCount,
-        imageLink: book.ImageLink,
-        language: book.Language,
-        categories: book.Categories,
-        isReading: book.IsReading,
-        progress: book.Progress,
-      };
-    }
+const dbGetFavoritedBook = async (userId, googleBooksId) => {
+  const book = await FavoritedBook.findOne({
+    where: { userId, googleBooksId },
   });
+  if (!book) throw { message: "Book doesn't exist", code: 404 };
+  return book;
+};
 
-const dbGetFavoritedBooks = (userId) =>
-  User.findOne({ where: { UserId: userId } }).then(
-    (user) =>
-      user &&
-      user.getFavoritedBooks().then((books) =>
-        books.map((book) => ({
-          googleBooksId: book.GoogleBooksId,
-          title: book.Title,
-          description: book.Description,
-          authors: book.Authors,
-          publisher: book.Publisher,
-          publishedDate: book.PublishedDate,
-          pageCount: book.PageCount,
-          averageRating: book.AverageRating,
-          ratingsCount: book.RatingsCount,
-          imageLink: book.ImageLink,
-          language: book.Language,
-          categories: book.Categories,
-          isReading: book.IsReading,
-          progress: book.Progress,
-        }))
-      )
-  );
+/**
+ * @description - Gets an array of a user's favorited books
+ * @function dbGetFavoritedBooks
+ * @param {number} userId - Id of the user requesting their favorites
+ * @throws - Throws an error if the user isn't found
+ * @returns {Array} - Returns an array of the user's favorite books
+ */
+const dbGetFavoritedBooks = async (userId) => {
+  const user = await User.findOne({ where: { userId } });
+  if (!user) throw { message: 'Cannot get favorites', code: 400 };
+  const favoritedBooks = await user.getFavoritedBooks();
+  return favoritedBooks;
+};
 
-const dbToggleReadingBook = (userId, googleBooksId, isReading, progress) =>
-  FavoritedBook.findOne({
-    where: { UserId: userId, GoogleBooksId: googleBooksId },
-  })
-    .then((favoritedBook) => {
-      if (favoritedBook) {
-        return favoritedBook.update({ IsReading: isReading, Progress: progress }).then((book) => ({
-          isReading: book.IsReading,
-          progress: parseInt(book.Progress),
-        }));
-      } else {
-        throw { message: 'Book must be saved as a favorite', code: 400 };
-      }
-    })
-    .catch((error) => {
-      throw error;
-    });
+/**
+ * @todo - Change this method so it uses favoritedBookId instead of googleBooksId
+ * @description - Updates a users favorited book isReading and progress property
+ * @function dbToggleReadingBook
+ * @param {number} userId
+ * @param {string} googleBooksId
+ * @param {boolean} isReading
+ * @param {number} progress
+ * @throws - Throws an error if the book is not in the user's favorites
+ * @returns {Object} - The updated favorite book
+ */
+const dbToggleReadingBook = async (userId, googleBooksId, isReading, progress) => {
+  const favoritedBook = await FavoritedBook.findOne({
+    where: { userId, googleBooksId },
+  });
+  if (!favoritedBook) throw { message: 'Book must be in your favorites', code: 400 };
+  const bookData = await favoritedBook.update({ isReading, progress });
+  bookData.progress = parseInt(bookData.progress);
+  return bookData;
+};
 
-const dbSetBookProgress = (userId, googleBooksId, progress) =>
-  FavoritedBook.findOne({
-    where: { UserId: userId, GoogleBooksId: googleBooksId },
-  })
-    .then((favoritedBook) => {
-      if (favoritedBook) {
-        return favoritedBook.update({ Progress: progress }).then((book) => ({
-          progress: parseInt(book.Progress),
-        }));
-      } else {
-        throw { message: 'Book must be saved as a favorite', code: 400 };
-      }
-    })
-    .catch((error) => {
-      throw error;
-    });
+/**
+ * @todo - Change this method so it uses favoritedBookId instead of googleBooksId
+ * @description - Sets the reading progress of the favorited book
+ * @function dbSetBookProgress
+ * @param {number} userId
+ * @param {string} googleBooksId
+ * @param {number} progress
+ * @throws - Throws an error if the book is not in the user's favorites
+ * @returns {Object}  - The updated favorite book
+ */
+const dbSetBookProgress = async (userId, googleBooksId, progress) => {
+  const favoritedBook = await FavoritedBook.findOne({
+    where: { userId, googleBooksId },
+  });
+  if (!favoritedBook) throw { message: 'Book must be in your favorites', code: 400 };
+  const bookData = await favoritedBook.update({ progress });
+  bookData.progress = parseInt(bookData.progress);
+  return bookData;
+};
 
-const dbSaveNote = (userId, googleBooksId, noteText) =>
-  FavoritedBook.findOne({
-    where: { UserId: userId, GoogleBooksId: googleBooksId },
-  })
-    .then((favoritedBook) => {
-      if (favoritedBook) {
-        return favoritedBook
-          .createNote({
-            UserId: userId,
-            Text: noteText,
-          })
-          .then((note) => ({
-            noteId: note.NoteId,
-            text: note.Text,
-            createdAt: note.createdAt,
-            updatedAt: note.updatedAt,
-          }));
-      } else {
-        throw new Error('Server error');
-      }
-    })
-    .catch((error) => {
-      throw error;
-    });
+/**
+ * @todo - Change this method so it uses favoritedBookId instead of googleBooksId this way we don't need userId
+ * @description - Saves a note associated with a favorited book
+ * @function dbSaveNote
+ * @param {number} userId
+ * @param {string} googleBooksId
+ * @param {string} text
+ * @returns {Object} - Returns a note object
+ */
+const dbSaveNote = async (userId, googleBooksId, text) => {
+  const favoritedBook = await FavoritedBook.findOne({
+    where: { userId, googleBooksId },
+  });
+  if (!favoritedBook) throw { message: 'Book must be in your favorites', code: 400 };
+  const note = await favoritedBook.createNote({ text });
+  if (!note) throw { message: 'Error saving note' };
+  return note;
+};
 
-const dbEditNote = (userId, noteId, noteText) =>
-  Note.findOne({ where: { UserId: userId, NoteId: noteId } })
-    .then((note) => {
-      if (note) {
-        return note.update({ Text: noteText }).then((updatedNote) => ({
-          noteId: updatedNote.NoteId,
-          text: updatedNote.Text,
-          createdAt: updatedNote.createdAt,
-          updatedAt: updatedNote.updatedAt,
-        }));
-      } else {
-        throw new Error('Server error');
-      }
-    })
-    .catch(() => {
-      throw new Error('Server error');
-    });
+/**
+ * @todo - We no longer need to use userId, change this where it's called in the controller as well as the payload in the frontend
+ * @description - Edits a note associated with a favorited book
+ * @function dbEditNote
+ * @param {number} userId - DEPRECATING USE OF userId
+ * @param {number} noteId
+ * @param {string} text
+ * @throw - Throws an error if the note isn't found
+ * @returns {Object} - Returns the updated note object
+ */
+const dbEditNote = async (userId, noteId, text) => {
+  const note = await Note.findOne({ where: { noteId } });
+  if (!note) throw { message: 'Error saving note' };
+  const updatedNote = await note.update({ text });
+  if (!note) throw { message: 'Error saving note' };
+  return updatedNote;
+};
 
-const dbDeleteNote = (userId, noteId) =>
-  Note.findOne({ where: { UserId: userId, NoteId: noteId } })
-    .then((note) => note.destroy())
-    .catch(() => {
-      throw new Error('Server error');
-    });
+/**
+ * @todo - We no longer need to use userId, change this where it's called in the controller as well as the payload in the frontend
+ * @description - Deletes a note by noteId associated with a favoritedBook
+ * @param {number} userId
+ * @param {number} noteId
+ * @throws - Throws an error if the note doesn't exist or fails to get deleted
+ */
+const dbDeleteNote = async (userId, noteId) => {
+  const note = await Note.findOne({ where: { noteId } });
+  if (!note) throw { message: 'Error deleting note' };
+  await note.destroy();
+};
 
-const dbGetNotes = async (userId, googleBooksId) =>
-  FavoritedBook.findOne({
-    where: { UserId: userId, GoogleBooksId: googleBooksId },
-  })
-    .then((favoritedBook) => {
-      if (favoritedBook) {
-        return favoritedBook.getNotes().then((notes) =>
-          notes.map((note) => ({
-            noteId: note.NoteId,
-            text: note.Text,
-            createdAt: note.createdAt,
-            updatedAt: note.updatedAt,
-          }))
-        );
-      } else {
-        throw new Error('Server error');
-      }
-    })
-    .catch((error) => {
-      throw error;
-    });
+/**
+ * @todo - Use favoritedBookId instead of googleBooksId so that the userId is no longer needed
+ * @description - Gets all notes associated with a favoritedBook which is associated with a user
+ * @param {number} userId
+ * @param {string} googleBooksId
+ * @returns {Array} - Returns an array of notes
+ */
+const dbGetNotes = async (userId, googleBooksId) => {
+  const favoritedBook = await FavoritedBook.findOne({
+    where: { userId, googleBooksId },
+  });
+  if (!favoritedBook) throw { message: 'Book must be in your favorites' };
+  const notes = await favoritedBook.getNotes();
+  return notes;
+};
 
 module.exports = {
   dbGetFavoritedBook,
